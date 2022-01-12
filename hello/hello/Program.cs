1105d9224426abbeb58b7638c5128186031dd8dd
NepEvg <eneputina@inbox.ru>
2022-01-12 10:28:32 +0500

    not first project

--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Oh, hi, Mark!");
+Console.WriteLine("~~~~~~~~~~~~~~~~");
 Console.ReadKey();
